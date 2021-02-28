ad478fc28531af01df801cda58caaa384afc8378
daniel <dpletzke@gmail.com>
2021-02-28 14:22:41 -0600

    remove views boilerplate

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const socketio = require("socket.io");
@@ -14,15 +12,9 @@ const io = socketio(server, { origins: '*:*', pingTimeout: 10000});
 
 const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 const mountRoutes = require("./routes/indexRoutes");
 mountRoutes(app, io);
