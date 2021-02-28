94d6538b622e4a156835e9b7777c9c82dbd47b27
daniel <dpletzke@gmail.com>
2021-02-28 04:37:03 -0600

    edit seeds to testing

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -8,7 +8,7 @@ exports.seed = async (knex) => {
   await knex.raw("ALTER SEQUENCE users_id_seq RESTART with 100;");
   const saltedPass = await bcrypt.hash("test", saltRounds);
   const seeds = [
-    { email: "test@test.com", password: saltedPass, cash: 10 },
+    { email: "test@test.com", password: saltedPass, cash: 100 },
     { email: "test1@test.com", password: saltedPass, cash: 500 },
     { email: "test2@test.com", password: saltedPass, cash: 10000 },
   ];
--- db/seeds/02_stocks.js
+++ db/seeds/02_stocks.js
@@ -2,7 +2,12 @@ exports.seed = async (knex) => {
   await knex("stocks").del();
   await knex.raw("ALTER SEQUENCE stocks_id_seq RESTART with 1000;");
 
-  const seeds = [{ ticker: "AAPL" }, { ticker: "TSLA" }, { ticker: "GME" }];
+  const seeds = [
+    { ticker: "AAPL" },
+    { ticker: "TSLA" },
+    { ticker: "GME" },
+    { ticker: "OPK" },
+  ];
 
   return await knex("stocks").insert(seeds);
 };
