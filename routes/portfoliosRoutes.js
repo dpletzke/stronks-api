dca1c120ebde8abc03f8b463620b70a72a97e911
daniel <dpletzke@gmail.com>
2021-02-27 19:26:47 -0600

    remove unnecessary promise wrappers

--- routes/portfoliosRoutes.js
+++ routes/portfoliosRoutes.js
@@ -16,9 +16,9 @@ module.exports = (controller) => {
     getStockPrices,
   } = controller;
 
-  /*
+  /**
    * get your portfolio
-   **/
+   */
   router.get("/", (req, res) => {
     const { userId } = req.query;
 
@@ -31,11 +31,12 @@ module.exports = (controller) => {
       });
   });
 
-  /*
-   * trade stock
-   * positive number is buying stock, negative is selling
-   * [{"ticker":"AAPL", "numOfShares": 200}]
-   **/
+/**
+ * trade stock
+ * trade: [{"ticker":"AAPL", "numShares": 200}]
+ * positive shares number is buying stock, negative is selling
+//  * positive trade value is buying stock, negative is selling
+ */
   router.post("/", (request, response, next) => {
     const { userId } = request.query;
     const { trade } = request.body;
@@ -46,73 +47,62 @@ module.exports = (controller) => {
 
     const newCashReq = Promise.all([cashReq, stockPricesReq]).then(
       ([cash, prices]) => {
-        return new Promise((resolve, reject) => {
-          console.log(cash, prices);
-          // find any tickers that aren't defined in the price response
-          const missingPrices = Object.keys(prices).filter((ticker) => {
-            return !prices[ticker];
-          });
-
-          if (missingPrices.length) {
-            console.log({ missingPrices });
-            reject(
-              new ErrorHandler(
-                404,
-                `Invalid Ticker(s): ${missingPrices.join(
-                  ", "
-                )} invalid or not supported`
-              )
-            );
-            return;
-          }
-
-          const tradeValue = trade.reduce((acc, stock) => {
-            return acc - stock.numOfShares * prices[stock.ticker];
-          }, 0);
-          console.log({ cash, tradeValue });
-          if (cash + tradeValue < 0) {
-            reject(
-              new ErrorHandler(403, `Invalid Transaction: Cannot afford trade`)
-            );
-            return;
-          }
-          resolve(cash + tradeValue);
+        // find any prices that are undefined in the price response
+        const missingPrices = Object.keys(prices).filter((ticker) => {
+          return !prices[ticker];
         });
+
+        if (missingPrices.length) {
+          throw new ErrorHandler(
+            404,
+            `Invalid Ticker(s): ${missingPrices.join(
+              ", "
+            )} invalid or not supported`
+          );
+        }
+
+        const tradeValue = trade.reduce((acc, stock) => {
+          return acc - stock.numShares * prices[stock.ticker];
+        }, 0);
+        if (cash + tradeValue < 0) {
+          throw new ErrorHandler(
+            403,
+            `Invalid Transaction: Cannot afford trade`
+          );
+        }
+        return cash + tradeValue;
       }
     );
+
     const newPortfolioReq = portfolioReq.then((portfolio) => {
-      return new Promise((resolve, reject) => {
-        console.log({ portfolio });
+      const invalidTrades = [];
+      const newPortfolio = {...portfolio};
+      
+      // add new shares number to portfolio or throw error with invalid trades
+      trade.forEach((tradeStock) => {
+        
+        // get share values
+        const { numShares: tradeShares, ticker } = tradeStock;
+        const ownedShares = newPortfolio[ticker]
+          ? portfolio[ticker].numShares
+          : null;
+        console.log({ ownedShares, tradeShares });
+        // track if selling more shares than owned and set new shares number
+        if (ownedShares + tradeShares < 0) invalidTrades.push(ticker);
+        if (ownedShares) newPortfolio[ticker] = ownedShares + tradeShares;
+        else newPortfolio[ticker] = tradeShares;
 
-        const invalidTrades = [];
-        const newPortfolio = Object.keys(trade).reduce((acc, tradeStock) => {
-          // get share values
-          const { numOfShares: tradeShares } = trade[tradeStock];
-          const ownedShares = portfolio[tradeStock]
-            ? portfolio[tradeStock].numOfShares
-            : null;
-
-          // track if selling more shares than owned and set new shares number
-          if (ownedShares + tradeShares < 0) invalidTrades.push(tradeStock);
-          if (ownedShares) acc[tradeStock] = ownedShares + tradeShares;
-          else acc[tradeStock] = tradeShares;
-
-          return acc;
-        }, {});
-
-        if (invalidTrades.length) {
-          reject(
-            new ErrorHandler(
-              403,
-              `Invalid Transaction(s): Cannot sell more shares than owned for ${invalidTrades.join(
-                ", "
-              )}`
-            )
-          );
-          return;
-        }
-        resolve(newPortfolio);
       });
+
+      if (invalidTrades.length) {
+        throw new ErrorHandler(
+          403,
+          `Invalid Transaction(s): Cannot sell more shares than owned for ${invalidTrades.join(
+            ", "
+          )}`
+        );
+      }
+      return newPortfolio;
     });
 
     Promise.all([newCashReq, newPortfolioReq])
@@ -120,7 +110,7 @@ module.exports = (controller) => {
         console.log({ newCash, newPortfolio });
         //upsert stuff
         //return new portfolio
-        response.json({ msg: "Success" });
+        response.json({ cash: newCash, portfolio: newPortfolio });
       })
       .catch((err) => {
         next(err);
