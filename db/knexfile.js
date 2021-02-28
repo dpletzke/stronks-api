4e589b1b6c09f28589cb1114fcd6aa643e9ad26f
daniel <dpletzke@gmail.com>
2021-02-28 04:38:32 -0600

    add defaults to connectoion for knex cli

--- db/knexfile.js
+++ db/knexfile.js
@@ -6,9 +6,9 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'postgres',
+      database: process.env.DB_NAME || 'stronks-api',
     },
     // debug:true,
     migrations: {
